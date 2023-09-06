fbf2dbe3d446e4fa6913b6b7b13088de2f136174
ricotek <ricoteck@gmail.com>
2023-09-06 17:35:25 -0400

    clean upmpostman script

--- postman/build.sh
+++ postman/build.sh
@@ -2,5 +2,5 @@
 set -x
 cd test
 newman --version
-newman run vps.coracare.$COMPONENT.postman_collection.json -e vps.coracare.$COMPONENT.postman_environment.json --env-var baseUri=https://$BITBUCKET_DEPLOYMENT_ENVIRONMENT-$COMPONENT.coracare.vitacarerx.com --env-var crmApiUri=https://$BITBUCKET_DEPLOYMENT_ENVIRONMENT-crm.coracare.vitacarerx.com/api --env-var userApiUri=https://$BITBUCKET_DEPLOYMENT_ENVIRONMENT-users.api.coracare.vitacarerx.com --disable-unicode --reporters -r htmlextra --reporter-htmlextra-title "Prescriptions API Report $BITBUCKET_DEPLOYMENT_ENVIRONMENT-ENVIRONMENT" $BITBUCKET_DEPLOYMENT_ENVIRONMENT --reporter-htmlextra-export newman/$COMPONENT.html
+newman run $COMPONENT.postman_collection.json -e $COMPONENT.postman_environment.json --env-var baseUri=https://$BITBUCKET_DEPLOYMENT_ENVIRONMENT-$COMPONENT.$DOMAIN--env-var crmApiUri=https://$BITBUCKET_DEPLOYMENT_ENVIRONMENT-crm.coracare.vitacarerx.com/api --env-var userApiUri=https://$BITBUCKET_DEPLOYMENT_ENVIRONMENT-users.api.$DOMAIN--disable-unicode --reporters -r htmlextra --reporter-htmlextra-title "Prescriptions API Report $BITBUCKET_DEPLOYMENT_ENVIRONMENT-ENVIRONMENT" $BITBUCKET_DEPLOYMENT_ENVIRONMENT --reporter-htmlextra-export newman/$COMPONENT.html
 set +x
